f91ad7f2e1d41fbfa201223ef81dd4fc51f969ad
buttaaaaaaaa <amarrion.white.student@madisonpsb.org>
2021-10-21 15:39:50 +0000

    platformer

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,7 +39,7 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
-        createPlatform(660, 540);
+        createPlatform(660, 610);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
