abd24ec2bd82765a66a36632309f3dc08b682704
buttaaaaaaaa <amarrion.white.student@madisonpsb.org>
2022-02-11 19:04:58 +0000

    first website

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,34 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circles;
+var circles = (5);
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);
+        }
+            
+            // TODO 3 / 8 : Call the drawCircle() function 
+            function drawCircle();
+            
+            drawCircle();
+            drawCircle();
+            drawCircle();
+            drawCircle();
+
+            /*
+            This Function is called 60 times/second producing 60 frames/second.
+            In each frame, for every circle, it should redraw that circle
+            and check to see if it has drifted off the screen.
+            */
         
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+            function drawCircle()
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,7 +60,7 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+                
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
